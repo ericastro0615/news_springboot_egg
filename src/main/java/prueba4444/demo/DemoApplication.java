6203fa94eba177c1c62dbcd957d0988b72a0acb4
ericastro0615@gmail.com <ericastro0615@gmail.com>
2023-08-22 19:43:30 -0300

    etapa configuracion lista

---
+++ src/main/java/prueba4444/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package prueba4444.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba4444/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba4444.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
